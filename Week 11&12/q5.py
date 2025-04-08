adbe8f89c73b25831b2d56f97553ac9d1127111c
Schwway <70726524+ze-krypton@users.noreply.github.com>
2025-04-08 04:02:10 -0700

    Delete Week 11&12 directory

--- Week 11&12/q5.py
+++
@@ -1,9 +0,0 @@
-
-dict1 = {"a": 1, "b": 2, "c": 3}
-dict2 = {"d": 4, "e": 5, "f": 6}
-
-merged_dict = dict1.copy()  
-merged_dict.update(dict2)  
-
-
-print("Merged Dictionary:", merged_dict)
--- Week 11&12/q6.py
+++
@@ -1,3 +0,0 @@
-square_dict={num: num**2 for num in range(1,16)}
-
-print(square_dict)
