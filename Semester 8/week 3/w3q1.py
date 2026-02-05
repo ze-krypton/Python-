b55a19ac930422dffdd86315fefe230755bf5771
Schwway <70726524+ze-krypton@users.noreply.github.com>
2026-02-05 16:34:15 +0530

    Delete Semester 8 directory

--- Semester 8/week 3/w3q1.py
+++
@@ -1,8 +0,0 @@
-Num= int(input("Enter a number : "))
-
-if Num%2==0 :
-    print("It is an even number")
-else:
-    print("It is an Odd number")
-
-    
--- Semester 8/week 3/w3q4.py
+++
@@ -1,3 +0,0 @@
-sentence = input("Enter a sentence: ")
-count =sum(1 for ch in sentence.lower() if ch in "aeiou")
-print(count)   
